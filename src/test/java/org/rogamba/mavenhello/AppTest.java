89ebf77690f5c5b6dcde6081eba2d20d85330c17
Rodrigo <gamba.lavin@gmail.com>
2019-10-13 14:03:41 -0500

    Initial commit

---
+++ src/test/java/org/rogamba/mavenhello/AppTest.java
@@ -0,0 +1,20 @@
+package org.rogamba.mavenhello;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
